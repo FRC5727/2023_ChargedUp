c60185acf33ef21dde2f15dd741aea30cba24b63
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2023-03-07 11:05:34 -0500

    Remove unused code from Fusion Swerve

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -181,22 +181,6 @@ public final class Constants {
     }
   }
 
-  public static final class AutoConstants { // TODO: The below constants are used in the example auto, and must be tuned
-                                            // to specific robot
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
-
-    /* Constraint for the motion profilied robot angle controller */
-    public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
-        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-  }
-
   public static final int highMaster = 8;
   public static final int lowerMaster = 9;
   public static final int highSlave = 10;
