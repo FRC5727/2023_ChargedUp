d6f44ee029fd64af3c3d7bc32db8913dd9988219
Omegabytes-Programmers <12714162@student.rcsnc.org>
2023-03-13 18:48:21 -0400

    leds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,6 +32,7 @@ public class RobotContainer {
   // Subsystems
   private final ArmSubsystem armSubsystem = new ArmSubsystem();
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  
   private final Swerve s_Swerve = new Swerve();
   private final Auto auto = new Auto(armSubsystem, intakeSubsystem, s_Swerve);
 
@@ -75,6 +76,11 @@ public class RobotContainer {
       }
       SmartDashboard.putData("Position chooser", positionChooser);
     }
+
+
+  
+ 
+ 
   }
 
   public Command getAutonomousCommand() {
