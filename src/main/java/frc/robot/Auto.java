489851c523b4237bd80100d38493ed0a4eaef292
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2023-03-14 03:53:51 -0400

    Update and standardize all auto paths

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -3,7 +3,6 @@ package frc.robot;
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,7 +11,6 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -23,35 +21,14 @@ import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.ArmSubsystem.Position;
 
-// TODO Expel cube without moving to ANY position
-// TODO Update DriverStation
-
 public class Auto {
-    private static final double autoMaxVel = 3.0;
-    private static final double autoMaxAccel = 2.0;
-
     private HashMap<String, Command> eventMap = new HashMap<>();
     private SwerveAutoBuilder autoBuilder;
 
     private final SendableChooser<Boolean> pieceChooser = new SendableChooser<>();
     private final SendableChooser<ArmSubsystem.Position> placeChooser = new SendableChooser<>();
     private final SendableChooser<ArmSubsystem.Position> placeChooser2 = new SendableChooser<>();
-
-    private Command toggleCommand(IntakeSubsystem s_Intake) {
-        return Commands.runOnce(() -> { if (!pieceChooser.getSelected()) s_Intake.toggleCube(); });
-    }
-
-    private Command placePieceCommand(IntakeSubsystem s_Intake, ArmSubsystem s_Arm, Supplier<Position> positionFunc)
-    {
-        return new IdleCommand(s_Intake)
-            .raceWith(
-                Commands.runOnce(() -> DriverStation.reportError("Moving arm", false))
-                    .andThen(new ArmCommand(s_Arm, positionFunc).withTimeout(7.0))
-                    .andThen(Commands.runOnce(() -> DriverStation.reportError("Arm done moving", false))))
-            .andThen(Commands.runOnce(() -> DriverStation.reportError("Running justPlaceCommand", true)))
-            .andThen(justPlaceCommand(s_Intake))
-            .andThen(Commands.runOnce(() -> DriverStation.reportError("placePieceCommand terminating", false)));
-    }
+    private final SendableChooser<ArmSubsystem.Position> placeChooser3 = new SendableChooser<>();
 
     private Command justPlaceCommand(IntakeSubsystem s_Intake)
     {
@@ -59,28 +36,21 @@ public class Auto {
     }
   
     public Auto(ArmSubsystem s_Arm, IntakeSubsystem s_Intake, Swerve s_Swerve) {
-        eventMap.put("Place cube low", new PlaceCommand(s_Intake));
-        eventMap.put("Place cube high",
-            new IdleCommand(s_Intake)
-                .raceWith(new ArmCommand(s_Arm, Position.GRID_HIGH).withTimeout(7.0))
-            .andThen(new PlaceCommand(s_Intake).withTimeout(2.0))
-            .andThen(new IdleCommand(s_Intake)
-                .raceWith(new ArmCommand(s_Arm, Position.CHASSIS))));
-        // eventMap.put("Place piece", 
-//            Commands.runOnce(() -> { if !s_Intake.toggleCube())
-        // toggleCommand(s_Intake).andThen(placePieceCommand(s_Intake, s_Arm, placeChooser::getSelected)));
-        eventMap.put("Place cube", placePieceCommand(s_Intake, s_Arm, placeChooser::getSelected));
-        eventMap.put("Place cone", Commands.runOnce(() -> s_Intake.toggleCube())
-            .andThen(placePieceCommand(s_Intake, s_Arm, placeChooser::getSelected)));
-        eventMap.put("Place second cube", justPlaceCommand(s_Intake));
-        eventMap.put("Place third cube", justPlaceCommand(s_Intake));
-        eventMap.put("Prepare to place second piece", new ArmCommand(s_Arm, placeChooser2::getSelected));
+        eventMap.put("Place first piece", 
+            Commands.runOnce(() -> { if (!pieceChooser.getSelected().booleanValue()) s_Intake.toggleCube(); })
+                .andThen(new ArmCommand(s_Arm, placeChooser::getSelected)
+                    .raceWith(new IdleCommand(s_Intake)))
+                .andThen(justPlaceCommand(s_Intake)));
+        eventMap.put("Move arm to second", new ArmCommand(s_Arm, placeChooser2::getSelected));
+        eventMap.put("Move arm to third", new ArmCommand(s_Arm, placeChooser3::getSelected));
+        eventMap.put("Eject cube", justPlaceCommand(s_Intake));
         eventMap.put("Chassis", new ArmCommand(s_Arm, Position.CHASSIS));
-        eventMap.put("Ground intake", new ArmCommand(s_Arm, Position.INTAKE_GROUND)
-            .alongWith(
-                Commands.runOnce(() -> { if (!s_Intake.isCube()) s_Intake.toggleCube(); })
-                .andThen(new WaitCommand(1.0))
-                .andThen(new IntakeCommand(s_Intake))));
+        eventMap.put("Ground intake",
+            new ArmCommand(s_Arm, Position.INTAKE_GROUND)
+                .alongWith(
+                    Commands.runOnce(() -> { if (!s_Intake.isCube()) s_Intake.toggleCube(); })
+                    .andThen(new WaitCommand(1.0))
+                    .andThen(new IntakeCommand(s_Intake))));
         eventMap.put("Balance", new AutoBalanceCommand(s_Swerve));
 
         autoBuilder = new SwerveAutoBuilder(
@@ -110,14 +80,19 @@ public class Auto {
         placeChooser2.addOption("Low", Position.GRID_LOW);
         placeChooser2.setDefaultOption("Chassis", Position.CHASSIS);
         SmartDashboard.putData("Second placement location", placeChooser2);
+        
+        placeChooser3.addOption("High (3)", Position.GRID_HIGH);
+        placeChooser3.addOption("Middle", Position.GRID_MID);
+        placeChooser3.addOption("Low", Position.GRID_LOW);
+        placeChooser3.setDefaultOption("Chassis", Position.CHASSIS);
+        SmartDashboard.putData("Third placement location", placeChooser3);
     }
 
     public Command buildCommand(String pathName) {
         if (pathName == null)
             return null;
 
-        List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(pathName, autoMaxVel, autoMaxAccel);
-
+        List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(pathName, PathPlanner.getConstraintsFromPath(pathName));
         return autoBuilder.fullAuto(pathGroup);
     }
 
