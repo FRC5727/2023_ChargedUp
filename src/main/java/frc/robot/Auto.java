f4d517be13894ac28c2b48e1ce8a5f87b1f3f689
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2023-03-15 00:57:28 -0400

    Rotate more quickly in autos

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -59,7 +59,7 @@ public class Auto {
             s_Swerve::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
             Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
             new PIDConstants(10, 0.0, 0.0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
-            new PIDConstants(2.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
+            new PIDConstants(3.5, 0.0, 0.0), // PID constants to correct for rotation error (used to create the rotation controller)
             s_Swerve::setModuleStates, // Module states consumer used to output to the drive subsystem
             eventMap,
             true, // Mirror Blue path to Red automatically
