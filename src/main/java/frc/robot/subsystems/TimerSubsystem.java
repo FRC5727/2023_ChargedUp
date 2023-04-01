40c0e56b67840316fbf8e82c886f044793baee6f
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2023-03-31 23:48:17 -0400

    created calebs stupid timer on smartdashboard bc hes blind

---
+++ src/main/java/frc/robot/subsystems/TimerSubsystem.java
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TimerSubsystem extends SubsystemBase {
+  /** Creates a new TimerSubsystem. */
+  public Timer timer;
+  public double gameTime;
+  public TimerSubsystem() {
+    timer = new Timer();
+  }
+
+  @Override
+  public void periodic() {
+    gameTime = timer.getMatchTime();
+    SmartDashboard.putNumber("Match TImer", gameTime);
+  }
+}
