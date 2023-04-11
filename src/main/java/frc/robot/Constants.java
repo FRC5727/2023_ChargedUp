19ad829f9714d36ba7fa494f13b3de688c6851be
Omegabytes-Programmers <omegabytesprogrammers@gmail.com>
2023-04-11 01:46:24 -0400

    Use vision to adjust robot pose

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -204,4 +204,24 @@ public final class Constants {
     public static final int stallMaxCube = 10;
     public static final int stallMaxCone = 20;  
   }
+
+  public static final class Vision {
+    public static final double[][] ONE_APRIL_TAG_LOOKUP_TABLE = {
+      // {distance in meters, x std deviation, y std deviation, r (in degrees) std deviation}
+      {0, 0.01, 0.01, 10},
+      {1.5, 0.01, 0.01, 10},
+      {3, 0.145, 1.20, 30},
+      {4.5, 0.75, 5.0, 90},
+      {6, 1.0, 8.0, 180}
+    };
+
+    public static final double[][] TWO_APRIL_TAG_LOOKUP_TABLE = {
+      // {distance in meters, x std deviation, y std deviation, r (in degrees) std deviation}
+      {0, 0.01, 0.01, 5},
+      {1.5, 0.02, 0.02, 5},
+      {3, 0.04, 0.04, 15},
+      {4.5, 0.1, 0.1, 30},
+      {6, 0.3, 0.3, 60}
+    };
+  }
 }
--- src/main/java/frc/robot/subsystems/RobotPosition.java
+++ src/main/java/frc/robot/subsystems/RobotPosition.java
@@ -1,17 +1,37 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.util.MultiLinearInterpolator;
 import frc.robot.Constants;
 import frc.robot.Dashboard;
+
+// TODO Enable and disable vision processing
+// TODO Vision subsystem, parsing JSON, with timestamp to avoid redundant updates?
+// TODO Ignore wildly divergent values (button to force accept?)
 
 public class RobotPosition extends SubsystemBase {
     public static boolean positionDebug = false;
 
+    private final MultiLinearInterpolator oneAprilTagLookupTable = new MultiLinearInterpolator(Constants.Vision.ONE_APRIL_TAG_LOOKUP_TABLE);
+
     private SwerveDrivePoseEstimator swervePose;
     private Swerve s_Swerve;
+    private NetworkTable limelightTable;
+    private NetworkTableEntry botposeEntry;
+    private NetworkTableEntry targetEntry;
+    private NetworkTableEntry targetPoseEntry;
 
     public RobotPosition(Swerve swerve) {
         this.s_Swerve = swerve;
@@ -20,6 +40,11 @@ public class RobotPosition extends SubsystemBase {
         swervePose = new SwerveDrivePoseEstimator(Constants.Swerve.swerveKinematics, s_Swerve.getYaw(), s_Swerve.getModulePositions(), new Pose2d());
 
         Dashboard.watchBoolean("Position debug", positionDebug, (val) -> positionDebug = val.booleanValue());
+        
+        limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+        botposeEntry = limelightTable.getEntry(DriverStation.getAlliance() == Alliance.Red ? "botpose_wpired" : "botpose_wpiblue");
+        targetEntry = limelightTable.getEntry("tv");
+        targetPoseEntry = limelightTable.getEntry("targetpose_cameraspace");
     }
 
     public Pose2d getPose() {
@@ -32,13 +57,42 @@ public class RobotPosition extends SubsystemBase {
 
     @Override
     public void periodic() {
-        swervePose.update(s_Swerve.getYaw(), s_Swerve.getModulePositions());
+        Pose2d robotPose = swervePose.update(s_Swerve.getYaw(), s_Swerve.getModulePositions());
+        // double maxXYError = 1.0;
+
+        boolean haveTarget = targetEntry.getDouble(0) > 0;
+        double[] llpose = botposeEntry.getDoubleArray(new double[7]);
+        if (haveTarget && llpose[0] > 0.0 && llpose[1] > 0.0) {
+            Pose2d visionPose = new Pose2d(llpose[0], llpose[1], Rotation2d.fromDegrees(llpose[5]));
+
+            // if (Math.abs(robotPose.getX() - visionPose.getX()) > maxXYError ||
+            //            Math.abs(robotPose.getY() - visionPose.getY()) > maxXYError) {
+            //     DriverStation.reportWarning("Rejecting vision data with excess error", false);
+            // } else {
+                double[] targetPose = targetPoseEntry.getDoubleArray(new double[6]);
+                double targetDistance = Math.sqrt(Math.pow(targetPose[0], 2) + Math.pow(targetPose[1], 2) + Math.pow(targetPose[2], 2));
+                double[] stddev = oneAprilTagLookupTable.getLookupValue(targetDistance);
+                if (positionDebug) {
+                    SmartDashboard.putNumber("Target distance", targetDistance);
+                }
+                swervePose.setVisionMeasurementStdDevs(VecBuilder.fill(stddev[0], stddev[1], Units.degreesToRadians(stddev[2])));
+                swervePose.addVisionMeasurement(visionPose, Timer.getFPGATimestamp() - (llpose[6] / 1000.0));
+                swervePose.addVisionMeasurement(robotPose, targetDistance);
+            // }
+        }
 
         if (positionDebug) {
-            Pose2d robotPose = swervePose.getEstimatedPosition();
+            // Get position again, in case it's been updated with vision data
+            robotPose = swervePose.getEstimatedPosition();
             SmartDashboard.putNumber("Pose X", robotPose.getX());
             SmartDashboard.putNumber("Pose Y", robotPose.getY());
             SmartDashboard.putNumber("Pose Angle", robotPose.getRotation().getDegrees());
+
+            SmartDashboard.putBoolean("Limelight target", haveTarget);
+            SmartDashboard.putNumber("Limelight pose X", llpose[0]);
+            SmartDashboard.putNumber("Limelight pose Y", llpose[1]);
+            SmartDashboard.putNumber("Limelight yaw", llpose[5]);
+            SmartDashboard.putNumber("Limelight delay", llpose[6]);
         }
     }
 }
